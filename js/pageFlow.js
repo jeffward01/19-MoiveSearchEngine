f6281791004941d3df96740f2ded7c85caae7ca3
Jeff Ward <jeffwardcoding@gmail.com>
2015-10-20 16:22:16 -0700

    v1.5

---
+++ js/pageFlow.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#step2").hide();
+    
+    
+    //Click handler
+    $("#go").on("click", function(){
+        
+        $("#step2").slideToggle(500);
+    });//End Click
+    
+    
+    
+});//End Ready
