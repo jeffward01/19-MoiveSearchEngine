8f6c4dec107cadd8a96d8dc3b33b123fd66199e9
Jeff Ward <jeffwardcoding@gmail.com>
2015-10-20 14:21:40 -0700

    working v1.2

--- js/app.js
+++ js/app.js
@@ -3,12 +3,23 @@ function MyViewModel() {
 
     self.movies = ko.observableArray();
     self.searchTerm = ko.observable();
+    
+    self.error = ko.observable(false);
+    self.errorMessage = ko.observable();
 
     self.searchForMovies = function () {
         $.getJSON("http://omdbapi.com/?s=" + self.searchTerm(), {
             api_key: '7a5035cca9022ea67734112eb921d5d33680a63d'
         }).done(function (data) {
-            ko.mapping.fromJS(data.Search, {}, self.movies);
+            self.error(false);
+            self.errorMessage('');
+            
+            if(data.Search) {
+                ko.mapping.fromJS(data.Search, {}, self.movies);   
+            } else {
+                self.error(true);
+                self.errorMessage(data.Error);
+            }
         });
     };
 };
